6c0e4aed0a83f790b1340d5e2064db8444b901fc
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2010-12-25 19:00:34 +0000

    refactored PDF creation to make easier for custom faceplate GUI.

--- Faceplate/create_faceplate.py
+++ Faceplate/create_faceplate.py
@@ -26,28 +26,37 @@ H = 9 * inch
 W = 12 * inch
 TOP = 1 * inch - 7.5 * mm
 LEFT = 1.5 * inch - 7.5 * mm
+XS = arange(LEFT, LEFT + dx * (N_COL + .01), dx)
+YS = arange(TOP, TOP + dy * (N_ROW + .01), dy) # from top
+assert len(XS) == N_COL + 1, '%s != %s ' % (len(XS), N_COL + 1)
+assert len(YS) == N_ROW + 1, '%s != %s ' % (len(YS), N_ROW + 1)
+YS = H - YS # from bottom
 
 DX = (W - 2 * LEFT) / float(N_COL)
-print DX, dx
 dx = DX
 
-def draw(filename, faceplate=True, baffle=True):
+class Image:
+    def __init__(self, filename, x, y, w, h):
+        self.filename = filename
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    def drawon(self, c):
+        im = PIL.Image.open(self.filename)
+        c.drawInlineImage(im, 
+                          self.x, self.y, self.w, self.h)
+
+def draw(filename, data, images, fontname='Asana-Math', fontsize=30,
+         faceplate=True, baffle=True):
     c = canvas.Canvas(filename,
                       pagesize=(W, H)
                       )
 
-    xs = arange(LEFT, LEFT + dx * (N_COL + .01), dx)
-    print xs[0] / inch
-    print (W - xs[-1]) / inch
-    ys = arange(TOP, TOP + dy * (N_ROW + .01), dy) # from top
-    assert len(xs) == N_COL + 1, '%s != %s ' % (len(xs), N_COL + 1)
-    assert len(ys) == N_ROW + 1, '%s != %s ' % (len(ys), N_ROW + 1)
-    ys = H - ys # from bottom
-
     c.setLineWidth(1/16. * inch)
     if baffle:
-        c.grid(xs, ys)
-        c.rect(xs[0], ys[-1], xs[-1] - xs[0], ys[0] - ys[-1])
+        c.grid(XS, YS)
+        c.rect(XS[0], YS[-1], XS[-1] - XS[0], YS[0] - YS[-1])
     hole_sepx = 2.920
     hole_sepy = 2.887
     startx = .172
@@ -65,7 +74,31 @@ def draw(filename, faceplate=True, baffle=True):
     c.setLineWidth(1/64. * inch)
     for x, y in mounts:
         c.circle(x*inch, y*inch, r*inch, fill=True)
-    data = [
+
+    t=Table(data, N_COL*[dx], N_ROW*[dy])
+    t.setStyle(TableStyle(
+            [('FONTNAME', (0, 0), (N_COL - 1, N_ROW - 1), fontname),
+             ('FONTSIZE', (0, 0), (N_COL - 1, N_ROW - 1), fontsize),
+             ('ALIGN', (0, 0), (N_COL - 1, N_ROW - 1), 'CENTRE'),
+             ]))
+    # elements = []
+    # elements.append(t)
+    # doc = SimpleDocTemplate("simple_table.pdf")
+    # doc.build(elements)
+    c.setFont("Times-Roman", 30)
+    print t.wrap(W, H)
+    if faceplate:
+        t.drawOn(c, XS[0], YS[-2] - dy/2.5)
+        #         bug_i = PIL.Image.open('Images/noun_project_198.png')
+        #        c.drawInlineImage(bug_i, 
+        #                  2.826 * inch, .05 * inch, .3*inch, .4*inch)
+        for im in images:
+            im.drawon(c)
+
+    c.showPage()
+    c.save()
+
+data = [
         ('I',"T'",'S','X','A','T','E','N','R','Q','U','A','R','T','E','R'),
         ('T','W','E','N','T','Y','F','I','V','E','D','P','A','S','T','O'),
         ('T','W','E','L','V','E','T','W','O','N','E','S','E','V','E','N'),
@@ -79,63 +112,14 @@ def draw(filename, faceplate=True, baffle=True):
         ('T','H','A','N','K','V','I','W','E','M','N','E','E','D','1','7'),
         ('Y','O','U','R','!','S','U','P','P','O','R','T','!','!','8','9')]
 
-    t=Table(data, N_COL*[dx], N_ROW*[dy])
-    t.setStyle(TableStyle(
-            [('FONTNAME', (0, 0), (N_COL - 1, N_ROW - 1), 'Asana-Math'),
-             ('FONTSIZE', (0, 0), (N_COL - 1, N_ROW - 1), 30),
-             ('ALIGN', (0, 0), (N_COL - 1, N_ROW - 1), 'CENTRE'),
-             ]))
-    # elements = []
-    # elements.append(t)
-    # doc = SimpleDocTemplate("simple_table.pdf")
-    # doc.build(elements)
-    c.setFont("Times-Roman", 30)
-    print t.wrap(W, H)
-    if faceplate:
-        t.drawOn(c, xs[0], ys[-2] - dy/2.5)
-        bug_i = PIL.Image.open('Images/noun_project_198.png')
-        # bug_i = PIL.Image.open('minute1.png')
-        c.drawInlineImage(bug_i, 
-                          2.826 * inch, .05 * inch, .3*inch, .4*inch)
-        image0 = PIL.Image.open('Images/hourglass0.png')
-        c.drawInlineImage(image0, 
-                          xs[-2] + dx * .35,
-                          ys[5] + dy * .2,
-                          dx * .3, 
-                          dy * .56
-                          )
-        image1 = PIL.Image.open('Images/hourglass1.png')
-        c.drawInlineImage(image1, 
-                          xs[-2] + dx * .35,
-                          ys[6] + dy * .2,
-                          dx * .3, 
-                          dy * .56
-                          )
-        image2 = PIL.Image.open('Images/hourglass2.png')
-        c.drawInlineImage(image2, 
-                          xs[-2] + dx * .315,
-                          ys[7] + dy * .2,
-                          dx * .3, 
-                          dy * .56
-                          )
-        image3 = PIL.Image.open('Images/hourglass3.png')
-        c.drawInlineImage(image3, 
-                          xs[-2] + dx * .315,
-                          ys[8] + dy * .2,
-                          dx * .3, 
-                          dy * .56
-                          )
-        image4 = PIL.Image.open('Images/hourglass4.png')
-        c.drawInlineImage(image4, 
-                          xs[-2] + dx * .315,
-                          ys[9] + dy * .2,
-                          dx * .3, 
-                          dy * .56
-                          )
+images = [Image('Images/noun_project_198.png',
+          2.826 * inch, .05 * inch, .3*inch, .4*inch)]
 
-    c.showPage()
-    c.save()
+for i in range(5):
+    images.append(Image('Images/hourglass%d.png' % i,
+          XS[-2] + dx * .35, YS[5 + i] + dy * .2,
+          dx * .3, dy * .56))
 
-draw("faceplate.pdf", faceplate=True, baffle=False)
-draw("baffle.pdf", faceplate=False, baffle=True)
-draw("both.pdf", faceplate=True, baffle=True)
+draw("faceplate.pdf", data, images, faceplate=True, baffle=False)
+draw("baffle.pdf", data, images, faceplate=False, baffle=True)
+draw("both.pdf", data, images, fontsize=30, faceplate=True, baffle=True)
