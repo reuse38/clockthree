759ffaa5ec064f74a7e0cba6166895704fbb41d7
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2011-01-07 01:23:30 +0000

    Broke out EDL EEPROM DATA LIBRARY into its own file.

---
+++ libraries/ClockTHREE/EDL.cpp
@@ -0,0 +1,149 @@
+#include <inttypes.h>
+#include "EDL.h"
+#include "EEPROM.h"
+
+const uint16_t MAX_EEPROM_ADDR = 1023;
+
+/* 
+ * Copy DID to data to dest.  
+ * dest -- location to copy DID record.  It must have enough space allocated to contain entire record.
+ * len_p -- ouput, if output is true, the value pointed to by this pointer will contian the length for this did
+ */
+bool did_read(uint8_t did, char *dest, uint8_t *len_p){
+  int16_t addr;
+  bool status = false;
+
+  if(get_did_addr(did, &addr, len_p)){
+    for(int i = 0; i < *len_p; i++){
+      dest[i] = EEPROM.read(addr + i);
+    }
+    status = true;
+  }
+  return status;
+}
+
+/* 
+ * Write a valid DID stored in data.
+ * return true if successful, false otherwise.
+ */
+bool did_write(char* data){
+  uint8_t n = EEPROM.read(MAX_EEPROM_ADDR);
+  int16_t addr = 0;
+  uint8_t did = data[0];
+  uint8_t len = data[1];
+  bool status = false;
+  
+  int16_t tmp_addr;
+  uint8_t tmp_l;
+  if(!get_did_addr(did, &tmp_addr, &tmp_l)){
+    if(did_next_addr(&addr)){
+      if(addr + len < MAX_EEPROM_ADDR - 1){
+	for(int i = 0; i < len; i++){
+	  EEPROM.write(addr + i, data[i]);
+	}
+	EEPROM.write(MAX_EEPROM_ADDR, n + 1);
+	status = true;
+      }
+    }
+  }
+  return status;
+}
+
+/*
+ * Get next available DID address.  
+ * Return true if space any space remains, otherwise false
+ */
+bool did_next_addr(int16_t *addr_p){
+  uint8_t n = EEPROM.read(MAX_EEPROM_ADDR);
+  bool status = false;
+  *addr_p = 0;
+
+  // go to end of the line of DIDs
+  for(int i = 0; 
+      i < n && *addr_p < MAX_EEPROM_ADDR - 2; 
+      i++){
+    *addr_p += EEPROM.read(*addr_p + 1);
+  }
+  if(*addr_p < MAX_EEPROM_ADDR - 2){
+    status = true;
+  }
+  else{
+    // strcpy(serial_msg, "mem");
+  }
+  return status;
+}
+
+/*
+ * delete data store under identifier did
+ * return true if successful, false otherwise
+ */
+bool did_delete(uint8_t did){
+  int16_t addr, next_addr;
+  uint8_t len;
+  uint8_t n = EEPROM.read(MAX_EEPROM_ADDR);
+  bool status = false;
+  if(get_did_addr(did, &addr, &len)){
+    status = true;
+    if(n == 1){
+      len = EEPROM.read(1);
+      for(int i = 0; i < len; i++){
+	EEPROM.write(i, 0);
+      }
+      EEPROM.write(MAX_EEPROM_ADDR, 0);
+    }
+    else{
+      if(did_next_addr(&next_addr)){
+	// Serial.print("HERE next_addr:");
+	// Serial.print(next_addr, DEC);
+	// Serial.print(" addr:");
+	// Serial.println(addr, DEC);
+	for(int i = addr; i < next_addr - 1 - len; i++){
+	  // Serial.print("    ");
+	  // Serial.print(i, DEC);
+	  EEPROM.write(i, EEPROM.read(i + len));
+	  // Serial.print(" ");
+	  // Serial.println(EEPROM.read(i + len), DEC);
+	}
+      }
+      // Serial.println("HERE2");
+      if(n > 0){
+	EEPROM.write(MAX_EEPROM_ADDR, n - 1);
+      }
+    }
+  }
+  return status;
+}
+
+/*
+ * Get the address of DID identified by did.
+ * did -- data id
+ * addr_p -- output, if output is true, the value pointed to by this pointer will contian the address for this did
+ * len_p -- output, if output is true, the value pointed to by this pointer will contian the length for this did
+ */
+bool get_did_addr(uint8_t did, int16_t* addr_p, uint8_t *len_p){
+  bool status = false;
+  uint8_t id;
+
+  *addr_p = 0;
+  id = EEPROM.read(*addr_p);
+  uint8_t n = EEPROM.read(MAX_EEPROM_ADDR);
+  uint8_t i = 0;
+  while((*addr_p < MAX_EEPROM_ADDR - 2) && 
+	(did != id) && 
+	(i++ < n)){
+    *len_p = EEPROM.read(*addr_p + 1);
+    *addr_p += *len_p;
+    id = EEPROM.read(*addr_p);
+  }
+  if(id == did){
+    // make sure it is not too long
+    if(*addr_p < MAX_EEPROM_ADDR - 2){
+      *len_p = EEPROM.read(*addr_p + 1);
+      status = true;
+    }
+  }
+  else{
+  }
+  return status;
+}
+
---
+++ libraries/ClockTHREE/EDL.h
@@ -0,0 +1,61 @@
+/*
+  EDL.h -- EEPROM DATA LIBRARY for Arduino.  
+  Data handling routines for easy assured access to EEPROM data.
+  Each record is reference by a Data ID or DID for short.  
+  The library has been optimized for storage (as opposed to fact access).
+
+  Justin Shaw
+  The hardware and software for ClockTHREE have been enabled by the 
+  open souce Peggy2.  Thanks to the Evil Mad Science Team for making them
+  available.
+  
+  LIBRARY VERSION: 0.01, DATED 1/6/2011
+
+Licenced under Creative Commons Attribution.
+Attribution 3.0 Unported
+*/
+
+#ifndef EDL_H
+#define ELD_H
+
+/* 
+ * EEPROM Map -- no gaps between DIDs!
+ *      DID: 0                 1       2     3     4   ...
+ *            |---------------->|------>|---->|---->|---...
+ * ADDR/LEN: 0/20              20/7    27/5  32/5  37/ ...  
+ * number of DIDs stored in last byte of EEPROM
+ */
+
+/*
+ * Get next available DID address.  
+ * Return true if space any space remains, otherwise false
+ */
+bool did_next_addr(int16_t *addr_p);
+
+/* 
+ * Copy DID to data to dest.  
+ * dest -- location to copy DID record.  It must have enough space allocated to contain entire record.
+ * len_p -- ouput, if output is true, the value pointed to by this pointer will contian the length for this did
+ */
+bool did_read(uint8_t did, char *dest, uint8_t *len_p);
+
+/* 
+ * Write a valid DID stored in data.
+ * return true if successful, false otherwise.
+ */
+bool did_write(char* data);
+
+/*
+ * Get the address of DID identified by did.
+ * did -- data id
+ * addr_p -- output, if output is true, the value pointed to by this pointer will contian the address for this did
+ * len_p -- output, if output is true, the value pointed to by this pointer will contian the length for this did
+ */
+bool get_did_addr(uint8_t did, int16_t* addr_p, uint8_t *len_p);
+
+/*
+ * delete data store under identifier did
+ * return true if successful, false otherwise
+ */
+bool did_delete(uint8_t did);
+#endif
