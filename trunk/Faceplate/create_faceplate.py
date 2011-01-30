913cba45de5476a32ae56b7c1e87c7abd64ae73b
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2011-01-30 22:19:46 +0000

    Experimenting with baffle design.

--- trunk/Faceplate/create_faceplate.py
+++ trunk/Faceplate/create_faceplate.py
@@ -19,10 +19,16 @@ from reportlab.lib.colors import pink, black, red, blue, green, white
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Table, TableStyle
 from numpy import arange
 
+LASER_THICKNESS = .01 * inch
 DEG = pi/180.
 DTHETA = 180/50.
-THETA_EXTRA = 0.
-thetas = arange(-THETA_EXTRA, 180 + THETA_EXTRA, DTHETA)
+STANDOFF_R = 3 * mm
+STRUT_W = .2 * inch
+MOUNT_R = STANDOFF_R + 2 * mm
+THETA_EXTRA = arccos((STRUT_W / 2) / MOUNT_R) / DEG
+
+THETAS = arange(-THETA_EXTRA, 180 + THETA_EXTRA, DTHETA)
+
 class Line:
     def __init__(self, p1, p2):
         self.p1 = array(p1, copy=True)
@@ -77,6 +83,15 @@ class MyPath:
             l[0] += dx
             l[1] += dy
 
+    def getBottom(self):
+        return min(l[1] for l in self.points)
+    def getTop(self):
+        return max(l[1] for l in self.points)
+    def getLeft(self):
+        return min(l[0] for l in self.points)
+    def getRight(self):
+        return max(l[0] for l in self.points)
+
     def toOpenScad(self, height):
         points = array(self.points / inch)
         print '''\
@@ -111,8 +126,7 @@ adobe2codec = {
     'MacRomanEncoding':'macroman',
     'MacExpert':'macexpert',
     'PDFDoc':'pdfdoc',
-    
-    }
+}
 
 
 dx = 9/15. * inch
@@ -121,14 +135,20 @@ N_COL = 16
 N_ROW = 12
 H = 9 * inch
 W = 12 * inch
-TOP = 1 * inch - 7.5 * mm
-LEFT = 1.5 * inch - 7.5 * mm
-XS = arange(LEFT, LEFT + dx * (N_COL + .01), dx)
-YS = arange(TOP, TOP + dy * (N_ROW + .01), dy) # from top
+
+XOFFSET = .125 * inch
+YOFFSET = .125 * inch
+
+BOTTOM = 1 * inch - 7.5 * mm 
+TOP = H - BOTTOM
+
+LEFT = 1.5 * inch - 7.5 * mm 
+XS = arange(LEFT, LEFT + dx * (N_COL + .01), dx) + XOFFSET
+YS = arange(TOP, TOP - dy * (N_ROW + .01), -dy) + YOFFSET
 
 assert len(XS) == N_COL + 1, '%s != %s ' % (len(XS), N_COL + 1)
 assert len(YS) == N_ROW + 1, '%s != %s ' % (len(YS), N_ROW + 1)
-YS = H - YS # from bottom
+# YS = H - YS # from bottom
 
 DX = (W - 2 * LEFT) / float(N_COL)
 dx = DX
@@ -146,9 +166,9 @@ class Image:
                           self.x, self.y, self.w, self.h)
 
 def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
-         faceplate=True, baffle=True, CFL=True):
+         faceplate=True, baffle=True, CFL=True, horizontal_baffles=False, vertical_baffles=False):
     c = canvas.Canvas(filename,
-                      pagesize=(W, H)
+                      pagesize=(W + 2 * XOFFSET, H + 2 * YOFFSET)
                       )
     c.setTitle("ClockTHREE Faceplate: %s" % fontname)
     c.setFont(fontname, fontsize)
@@ -160,9 +180,9 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
     c.setLineWidth(1/64. * inch)
     hole_sepx = 2.920 * inch
     hole_sepy = 2.887 * inch
-    startx = .172 * inch
-    starty = .172 * inch
-    inside_r = 3 * mm
+    startx = .172 * inch + XOFFSET
+    starty = .172 * inch + YOFFSET
+
     mounts = [] # lower left
     for i in range(5):          
         if i != 1:
@@ -191,7 +211,7 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         for x in XS[:-1]:
             for y in YS[1:]:
                 # c.rect(x + lw, y + lw, dx - 2 * lw, dy -2 * lw)
-                # c.circle(x + dx/2., y + dy/2., 5*mm)
+                # c.circle(x + dx / 2., y + dy / 2., 5*mm)
                 pass
         
         # bug
@@ -200,9 +220,7 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         # c.rect(W - .75*inch, H - (3.3875 + .5)*inch, .5*inch, .5*inch)
      
         # w = r + 2 * mm
-        outside_r = inside_r + 1 * mm
         # w = .1 * inch
-        w = outside_r
 
         grid_SW = array([min(XS), min(YS)])
         grid_SE = array([max(XS), min(YS)])
@@ -212,24 +230,24 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         # Middle bottom
         # p = c.beginPath()
         p = MyPath()
-        # first = ((W + w)/2., grid_NW[1] + w)
-        # first = mounts[1][0] + w/2, grid_SE[1] - w
-        first = mounts[1][0] + w, grid_SE[1] - w
+        # first = ((W + STRUT_W / 2)/2., grid_NW[1] + STRUT_W / 2)
+        # first = mounts[1][0] + w/2, grid_SE[1] - STRUT_W / 2
+        first = mounts[1][0] + STRUT_W / 2, grid_SE[1] - STRUT_W
         p.moveTo(*first)
         
         if True: # right lower middle SSE
-            next = mounts[2][0] - w, grid_SW[1]- w
+            next = mounts[2][0] - STRUT_W / 2, grid_SW[1]- STRUT_W
             p.lineTo(*next)
-            # next = mounts[2][0] - w, mounts[2][1]
+            # next = mounts[2][0] - STRUT_W / 2, mounts[2][1]
             p.lineTo(*next)
 
-            for theta in (thetas - 180) * DEG:
-                next = mounts[2] + [outside_r * cos(theta), 
-                                    outside_r * sin(theta)]
+            for theta in (THETAS - 180) * DEG:
+                next = mounts[2] + [MOUNT_R * cos(theta), 
+                                    MOUNT_R * sin(theta)]
                 p.lineTo(*next)
         else:
             next = first
-        next = next[0], grid_SE[1] - w
+        next = next[0], grid_SE[1] - STRUT_W 
         p.lineTo(*next)
 
         # SE corner
@@ -238,32 +256,32 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         V /= d
         Vperp = array([-V[1], V[0]])
 
-        l1 = Line(grid_SE, grid_SW) - array([0, w])
-        l2 = Line(grid_SE, SE) - Vperp * w
+        l1 = Line(grid_SE, grid_SW) - array([0, STRUT_W ])
+        l2 = Line(grid_SE, SE) - Vperp * STRUT_W / 2
         next = l1.intersect(l2)
         p.lineTo(*next)
 
-        for theta in (-thetas + 180) * DEG:
+        for theta in (-THETAS + 180) * DEG:
         # for theta in arange(180 + THETA_EXTRA, 0 - THETA_EXTRA, -DTHETA) * DEG:
-            next = SE + (outside_r * sin(theta) * V + 
-                         outside_r * cos(theta) * Vperp)
+            next = SE + (MOUNT_R * sin(theta) * V + 
+                         MOUNT_R * cos(theta) * Vperp)
             p.lineTo(*next)
 
-        l1 = Line(grid_SE, grid_NE) + array([w, 0])
-        l2 = Line(grid_SE, SE) + Vperp * w
+        l1 = Line(grid_SE, grid_NE) + array([STRUT_W, 0])
+        l2 = Line(grid_SE, SE) + Vperp * STRUT_W / 2
         next = l1.intersect(l2)
         p.lineTo(*next)
         
         if True: # right side
             for m_i in [4, 5]:
-                next = grid_SE[0] + w, mounts[m_i][1] - w
+                next = grid_SE[0] + STRUT_W, mounts[m_i][1] - STRUT_W / 2
                 p.lineTo(*next)
-                next = mounts[m_i][0], mounts[m_i][1] - w
-                for theta in (thetas -90) * DEG:
-                    next = mounts[m_i] + [outside_r * cos(theta), 
-                                          outside_r * sin(theta)]
+                next = mounts[m_i][0], mounts[m_i][1] - STRUT_W / 2
+                for theta in (THETAS -90) * DEG:
+                    next = mounts[m_i] + [MOUNT_R * cos(theta), 
+                                          MOUNT_R * sin(theta)]
                     p.lineTo(*next)
-                next = grid_SE[0] + w, mounts[m_i][1] + w
+                next = grid_SE[0] + STRUT_W, mounts[m_i][1] + STRUT_W / 2
                 p.lineTo(*next)
 
         V = NE - grid_NE
@@ -271,29 +289,29 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         V /= d
         Vperp = array([-V[1], V[0]])
 
-        l1 = Line(grid_NE, grid_SE) + [w, 0]
-        l2 = Line(grid_NE, NE) - Vperp * w
+        l1 = Line(grid_NE, grid_SE) + [STRUT_W, 0]
+        l2 = Line(grid_NE, NE) - Vperp * STRUT_W / 2
         next = l1.intersect(l2)
         p.lineTo(*next)
-        for theta in (-thetas + 180) * DEG:
-            next = NE + (outside_r * sin(theta) * V + 
-                         outside_r * cos(theta) * Vperp)
+        for theta in (-THETAS + 180) * DEG:
+            next = NE + (MOUNT_R * sin(theta) * V + 
+                         MOUNT_R * cos(theta) * Vperp)
             p.lineTo(*next)
-        l1 = Line(grid_NW, grid_NE) + [0, w]
-        l2 = Line(grid_NE, NE) + Vperp * w
+        l1 = Line(grid_NW, grid_NE) + [0, STRUT_W]
+        l2 = Line(grid_NE, NE) + Vperp * STRUT_W / 2
         next = l1.intersect(l2)
         p.lineTo(*next)
         
         for i in [7, 8, 9]: #  gets each hole in top
-            next = mounts[i][0] + w, l1.p1[1]
+            next = mounts[i][0] + STRUT_W / 2, l1.p1[1]
             p.lineTo(*next)
-            for theta in (thetas) * DEG:
+            for theta in (THETAS) * DEG:
             # for theta in arange(0 - THETA_EXTRA, 180 + THETA_EXTRA, DTHETA) * DEG:
-                next = mounts[i] + [outside_r * cos(theta),  
-                                    outside_r * sin(theta)]
+                next = mounts[i] + [MOUNT_R * cos(theta),  
+                                    MOUNT_R * sin(theta)]
                 p.lineTo(*next)
 
-            next = mounts[i][0] - w, l1.p1[1]
+            next = mounts[i][0] - STRUT_W / 2, l1.p1[1]
             p.lineTo(*next)
         
         V = NW - grid_NW
@@ -301,30 +319,30 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         V /= d
         Vperp = array([-V[1], V[0]])
 
-        l1 = Line(grid_NW, grid_NE) + [0, w]
-        l2 = Line(NW, grid_NW) - w * Vperp
+        l1 = Line(grid_NW, grid_NE) + [0, STRUT_W]
+        l2 = Line(NW, grid_NW) - STRUT_W / 2 * Vperp
         next = l1.intersect(l2)
         p.lineTo(*next)
-        for theta in (-thetas + 180) * DEG:
-            next = NW + (outside_r * sin(theta) * V + 
-                         outside_r * cos(theta) * Vperp)
+        for theta in (-THETAS + 180) * DEG:
+            next = NW + (MOUNT_R * sin(theta) * V + 
+                         MOUNT_R * cos(theta) * Vperp)
             p.lineTo(*next)
             
-        l1 = Line(grid_NW, grid_SW) - array([w, 0])
-        l2 = l2 + 2 * w * Vperp
+        l1 = Line(grid_NW, grid_SW) - array([STRUT_W, 0])
+        l2 = l2 + 2 * STRUT_W / 2 * Vperp
         next = l2.intersect(l1)
         p.lineTo(*next)
 
         if True: # left side
             for i in [12, 13]:
-                next = [grid_NW[0] - w, mounts[i][1] + w]
+                next = [grid_NW[0] - STRUT_W, mounts[i][1] + STRUT_W / 2]
                 p.lineTo(*next)
-                for theta in (thetas + 90) * DEG:
+                for theta in (THETAS + 90) * DEG:
                 # for theta in arange(90 - THETA_EXTRA, 270 + THETA_EXTRA, DTHETA) * DEG:
-                    next = mounts[i] + [outside_r * cos(theta),  
-                                        outside_r * sin(theta)]
+                    next = mounts[i] + [MOUNT_R * cos(theta),  
+                                        MOUNT_R * sin(theta)]
                     p.lineTo(*next)
-                next = grid_NW[0] - w, mounts[i][1] - w
+                next = grid_NW[0] - STRUT_W, mounts[i][1] - STRUT_W / 2
                 p.lineTo(*next)
 
         V = SW - grid_SW
@@ -332,25 +350,25 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
         V /= d
         Vperp = array([-V[1], V[0]])
 
-        l1 = Line(grid_NW, grid_SW) - array([w, 0])
-        l2 = Line(grid_SW, SW) - w * Vperp
+        l1 = Line(grid_NW, grid_SW) - array([STRUT_W, 0])
+        l2 = Line(grid_SW, SW) - STRUT_W / 2 * Vperp
         next = l2.intersect(l1)
         p.lineTo(*next)
-        for theta in (-thetas + 180) * DEG:
-            next = SW + (outside_r * sin(theta) * V + 
-                         outside_r * cos(theta) * Vperp)
+        for theta in (-THETAS + 180) * DEG:
+            next = SW + (MOUNT_R * sin(theta) * V + 
+                         MOUNT_R * cos(theta) * Vperp)
             p.lineTo(*next)
 
-        l1 = Line(grid_SW, grid_SE) - array([0, w])
-        l2 = l2 + 2 * w * Vperp
+        l1 = Line(grid_SW, grid_SE) - array([0, STRUT_W])
+        l2 = l2 + 2 * STRUT_W / 2 * Vperp
         next = l1.intersect(l2)
         p.lineTo(*next)
-        next = mounts[1][0] - w, grid_SW[1] - w
+        next = mounts[1][0] - STRUT_W / 2, grid_SW[1] - STRUT_W
         p.lineTo(*next)
         
-        for theta in (thetas + 180) * DEG:
-            next = mounts[1] + [outside_r * cos(theta),  
-                                outside_r * sin(theta)]
+        for theta in (THETAS + 180) * DEG:
+            next = mounts[1] + [MOUNT_R * cos(theta),  
+                                MOUNT_R * sin(theta)]
             p.lineTo(*next)
 
         p.lineTo(*first)
@@ -361,7 +379,7 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
     if baffle:
         c.setLineWidth(1/64. * inch)
         for x, y in mounts:
-            c.circle(x, y, inside_r, fill=False)
+            c.circle(x, y, STANDOFF_R, fill=False)
     else:
         face_mounts = [mounts[0], mounts[1], mounts[3],
                        mounts[6], mounts[8], mounts[11]]
@@ -401,18 +419,40 @@ def draw(filename, data, images, fontname='Times-Roman', fontsize=30,
             c.line(XS[-2] + dx/10., YS[-1] + dx/10.,
                    XS[-1] - dx/10., YS[-2] - dy/10.)
 
-    if False: # check baffle grid size
+    if True: # check baffle grid size
         import create_baffle_grid
         c.setLineWidth(1/64. * inch)
-        baffle = create_baffle_grid.create_baffle(8 * mm,
-                                                   .1 * inch, 15, dx)
-        baffle.translate(XS[0], YS[-2])
-        baffle.drawOn(c)
-        baffle = create_baffle_grid.create_baffle(8 * mm,
-                                                   .1 * inch, 11, dy)
-        baffle.rotate(90)
-        baffle.translate(XS[1], YS[0])
-        baffle.drawOn(c)
+        baffle_height = 10.00 * mm
+        if horizontal_baffles:
+            baffle = create_baffle_grid.create_baffle(baffle_height,
+                                                      .1 * inch, 15, dx)
+            baffle.translate(XS[0], YS[-1] - baffle_height)
+            for i in range(0):
+                baffle.translate(0, baffle_height)
+                baffle.drawOn(c)
+            baffle = create_baffle_grid.create_baffle(baffle_height,
+                                                      .1 * inch, 15, dx,
+                                                      tab_width=.1 * inch)
+            theta = 15 * DEG
+            baffle.rotate(theta / DEG)
+            ystep = baffle_height / cos(theta) + LASER_THICKNESS
+            baffle.translate(XS[0] + .1 * inch, YS[-1] - baffle.getBottom() - ystep + .05 * inch)
+            for i in range(11):
+                baffle.translate(0, ystep)
+                baffle.drawOn(c)
+
+
+        if vertical_baffles:
+            baffle = create_baffle_grid.create_baffle(baffle_height,
+                                                      .1 * inch, 11, dy,
+                                                      STRUT_W)
+            # baffle.rotate(90)
+            # baffle.translate(XS[0] - baffle_height, YS[0])
+            ystep = baffle_height + LASER_THICKNESS
+            baffle.translate(XS[0] + STRUT_W, YS[-1] - baffle_height + LASER_THICKNESS)
+            for i in range(15):
+                baffle.translate(0, ystep)
+                baffle.drawOn(c)
     c.showPage()
     c.save()
     print "Wrote %s." % filename
@@ -474,15 +514,16 @@ images = [Image('Images/NounProject/noun_project_198.png',                      
 ]
 
 for i in range(5):
-    continue
     images.append(Image('Images/hourglass%d.png' % i,
           XS[-2] + dx * .35, YS[5 + i] + dy * .25,
           dx * .3, dy * .56 ))
 
 def test():    
-    draw("baffle.pdf", data, images, faceplate=False, baffle=True)
+    draw("baffle_h.pdf", data, images, faceplate=False, baffle=True, horizontal_baffles=True)
+    draw("baffle_v.pdf", data, images, faceplate=False, baffle=True, vertical_baffles=True)
     # draw("both.pdf", data, images, fontname='FontdinerSwanky', faceplate=True, baffle=True)
-    draw("both.pdf", data, images, fontname='Ubuntu-Bold', faceplate=True, baffle=True)
+    draw("all.pdf", data, images, fontname='Ubuntu-Bold', faceplate=True, 
+         baffle=True, vertical_baffles=True, horizontal_baffles=True)
     # draw("bangla.pdf", bangla, images, fontname='ShadheenLipi', faceplate=True, baffle=False)
 
 def main(fontnames):
