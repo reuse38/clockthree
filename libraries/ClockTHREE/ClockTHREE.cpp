931aeba118fbfdf92d063d1f34c3cd430f2cc86e
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2011-03-04 20:53:07 +0000

    added library files

--- libraries/ClockTHREE/ClockTHREE.cpp
+++ libraries/ClockTHREE/ClockTHREE.cpp
@@ -381,11 +381,13 @@ void ClockTHREE::off(){
 }
 
 void ClockTHREE::note(uint16_t freq){
+  return;
   MsTimer2::stop();
   tone(SPEAKER_PIN, freq);
 }
 
 void ClockTHREE::nonote(){
+  return;
   noTone(SPEAKER_PIN);
   MsTimer2::start();
 }
