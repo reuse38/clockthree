573ed7d5a3253d8a8bf056b5ff92b6ff947bdb43
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2011-05-21 02:41:21 +0000

    added german and C3jr support

---
+++ trunk/libraries/ClockTHREE/mem_font.h
@@ -0,0 +1,10 @@
+#include <avr/pgmspace.h>
+const uint8_t N_CHAR = 128;
+class MemFont{
+ public:
+  /*
+    Place character into output buffer out in selected color.
+   */
+  void getChar(char letter, uint8_t color, uint32_t* out);
+ private:
+};
