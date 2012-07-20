d684c75aa8ebccf890d979866249501bdd342d6a
wyojustin <wyojustin@f2ec0ed3-786c-2516-f35b-d4ee5b3d3c00>
2012-07-20 01:09:14 +0000

    This finaly seems to work.  Added watchdog timer.  Updates every hour.

--- trunk/TimeZ/TimeZ.ino
+++ trunk/TimeZ/TimeZ.ino
@@ -3,18 +3,12 @@
 #include "Time.h"
 #include "Wire.h"
 #include "SD.h"
+#include <avr/wdt.h>
 // #include "SPI.h"
 
-// Arduino 1.0 compatibility
-#if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #define WIRE_READ Wire.read();
 #include "SoftwareSerial.h"
-#else
-#include "WProgram.h"
-#define WIRE_READ Wire.receive()
-// #include "NewSoftSerial.h"
-#endif
 
 #include <TinyGPS.h>
 
@@ -24,12 +18,14 @@ volatile unsigned long count = 0;
 volatile unsigned long pps_start_us = 0;
 volatile unsigned long pps_tick_us = 1000000;  // filtered
 volatile unsigned long _pps_tick_us = 1000000; // unfiltered
-volatile double lat = 1000; // default to bad lat
-volatile double lon = 1000; // default to bad lon
+volatile float lat = 1000; // default to bad lat
+volatile float lon = 1000; // default to bad lon
 
 volatile unsigned long rtc_start_us = 0;          
 volatile unsigned long rtc_tick_us = 1000000;  // filtered
 volatile unsigned long _rtc_tick_us = 1000000; // unfiltered
+int rtc_set_hour = -1;
+bool dst_flag;
 
 unsigned long sync_time = 0;     // seconds of last GPS 1pps sync
 unsigned long last_set_time = 0; // seconds of last GPS time set
@@ -59,12 +55,7 @@ const uint8_t chipSelect = 10;
 const uint8_t MY_ADDR = 121;
 
 TinyGPS gps;
-#if defined(ARDUINO) && ARDUINO >= 100
-// SoftwareSerial sws(5, 4);
 SoftwareSerial sws(6, A7);
-#else
-//NewSoftSerial sws(5, 4);
-#endif
 
 void ERROR_OUT(uint8_t n_blink){
   detachInterrupt(0);
@@ -92,9 +83,8 @@ void pps_interrupt(){
 void rtc_interrupt(){
   unsigned long  now_us = micros();
   long drift_us;
-  // Serial.println("rtc_interrupt");
   sqw_led_state = !sqw_led_state;
-  // digitalWrite(PPS_LED, sqw_led_state);
+  // digitalWrite(SQW_LED, sqw_led_state);
   rtc_start_us = now_us;
 }
 
@@ -111,15 +101,14 @@ time_t Serial_to_time(char *in){
 }
 
 void setup(){
-  gps.add_callback(grab_datetime);
   sws.begin(9600);
-
   Serial.begin(115200);
   Serial.println("TimeZ v1.0");
-  //\\Serial.println("Copyright WyoLum, LLC, 2012");
+  Serial.println("Copyright WyoLum, LLC, 2012");
+  wdt_enable(WDTO_8S);
 
   Wire.begin(MY_ADDR);
-  // c3.init();
+
   // From MaceTech.com: set 1Hz reference square wave
   Wire.beginTransmission(0x68); // address DS3231
   WIRE_WRITE1(0x0E); // select register
@@ -139,27 +128,20 @@ void setup(){
   digitalWrite(SQW_LED, LOW);
   // setRTC(2010, 1, 1, 0, 0, 0); // TODO: DELETE THIS LINE!
   set_1Hz_ref(getTime(), SQW_PIN, rtc_interrupt, FALLING);
-  //\\Serial.println("Waiting for 1Hz signal");
+
   while(rtc_start_us == 0){
     delay(100);
   }
-  //\\Serial.println("1Hz signal found");
   attachInterrupt(PPS_PIN - 2, pps_interrupt, RISING);
 
-  //\\Serial.print("now():");
-  //\\Serial.println(now());
-
-  //\\Serial.print("Initializing SD card...");
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
   // Note that even if it's not used as the CS pin, the hardware SS pin 
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
   // or the SD library functions will not work. 
    pinMode(chipSelect, OUTPUT);
   if (!SD.begin(chipSelect)) {
-    //\\Serial.println("initialization failed!");
     ERROR_OUT(4);
   }
-  //\\Serial.println("initialization done.");
   initialize_clock();
 }
 
@@ -169,25 +151,6 @@ void setup(){
 
   format:StandardTime, Dailight Time 4-byte unsigned
  */
-void restore_tz(){
-  char *buff;
-  int16_t st;
-  int16_t dst;
-  File tz_file;
-  unsigned long start_s;
-  
-  if(abs(lat) < 90 and abs(lon) < 360){
-    // 2. lat / lon valid
-    gmt_offset(lat, lon, &tz_offset_st, &tz_offset_dst);
-  }
-  // Europe DST rule
-  // last Sunday in March to last sun in October at 01:00
-  
-  // Americas  DST from 
-  // 2nd Sunday in March to first Sunday in November at 02:00
-  //\\Serial.println(tz_offset_st);
-}
-
 int readshort(File myFile){
   int16_t out;
   char *dat = (char*)&out;
@@ -290,7 +253,7 @@ bool is_dst(uint16_t year, time_t t, float lon){
 
   if(dst_file){
     year = year % 2000;
-    dst_file.seek(year * 16 + 8 * (lon > -20)); // lat > -20 --> EU, else US 
+    dst_file.seek((year + 0) * 16  + 8 * (lon > -20)); // lat > -20 --> EU, else US 
     start = readtime(dst_file);
     stop = readtime(dst_file);
     out = (start < t) && (t < stop);
@@ -303,35 +266,27 @@ void initialize_clock(){
   // insure file is large enough
 }
 
-/*
-  GPS time is GMT, 
-  Arduino time is local, 
-  RTC time is local
- */
-void grab_datetime(unsigned long _date, 
-		   unsigned long _time,
-		   long _lat,
-		   long _lon,
-		   long alt,
-		   unsigned long speed,
-		   unsigned long course){
-  int YY, MM, DD, hh, mm, ss;
-  int nYY, nMM, nDD, nhh, nmm, nss;
-  unsigned long date = _date;
-  unsigned long time = _time;
-  int16_t offset;
-  boolean dst_flag;
-  int16_t my_offset;
+bool gps_is_active(){
+  unsigned long  now_us = micros();
+  return (now_us > pps_start_us) && (now_us - pps_start_us) < 1.5e6;
+}
+bool rtc_is_set(){
+  return hour() == rtc_set_hour;
+}
 
-  // set globals
-  if((abs(lon - _lon / 1e5) > .1) or (abs(lat - _lat / 1e5) > .1)){
-    lat = _lat / 1e5;
-    lon = _lon / 1e5;
-    restore_tz();
-  }
-  gmt_offset(lat, lon, &tz_offset_st, &tz_offset_dst);
-  // ck if DST applies
-  dst_flag = is_dst(year(), getTime(), lon);
+long local_offset(int yyyy, time_t now, float flat, float flon){
+  dst_flag = is_dst(yyyy, now, lon);
+  long my_offset;
+  gmt_offset(flat, flon, &tz_offset_st, &tz_offset_dst);
+  /* Serial.println(" "); */
+  /* Serial.print(flat); */
+  /* Serial.print(" "); */
+  /* Serial.print(flon); */
+  /* Serial.print(" "); */
+  /* Serial.print(tz_offset_st); */
+  /* Serial.print(" "); */
+  /* Serial.print(tz_offset_dst); */
+
   if(dst_flag){
     digitalWrite(SQW_LED, HIGH);
     my_offset = tz_offset_dst;
@@ -340,85 +295,110 @@ void grab_datetime(unsigned long _date,
     digitalWrite(SQW_LED, LOW);
     my_offset = tz_offset_st;
   }
-  time_t gmt = now() + my_offset;
-  nYY = YY = year(gmt);
-  nMM = MM = month(gmt);
-  nDD = DD = day(gmt);
-  hh = hour(gmt);
-  mm = minute(gmt);
-  ss = second(gmt);
-
-
-  nhh = time / 1000000;
-  time = time % 1000000;
-  nmm = time / 10000;
-  time = time % 10000;
-  nss = time / 100;
-  
-  //\\Serial.print("LAT:");
-  //\\Serial.println(lat);
-  //\\Serial.print("LON:");
-  //\\Serial.println(lon);
-  if(date > 0){
-    nDD = date / 10000;
-    date = date % 10000;
-    nMM = date / 100; 
-    date = date % 100;
-    nYY = 2000 + date;
-  }
-  //setRTC(YY, MM, DD, hh, mm, ss);
-
-  if(YY != nYY || MM != nMM || DD != nDD ||
-     hh != nhh || mm != nmm || abs(ss- nss) > 10){
-    setTime(nhh,nmm,nss,nDD,nMM,nYY);
-    setRTC(now() + my_offset);
-    last_set_time = getTime();
-    //setTime(last_set_time);
-  }
-  // Serial.print(nhh);
-  // Serial.print("hour():");
-  // Serial.println(hour());
   // Serial.print(" ");
-  // Serial.print(getTime());
-  // Serial.print(" ");
-  // Serial.println(lon);
-  //  Serial.print(" ");
-  // Serial.println(is_dst(year(), getTime(), lon));
+  // Serial.print(my_offset);
+  // Serial.println(" ");
+  return my_offset;
 }
-
 void loop(){
+  unsigned long age;
+  int Year;
+  byte Month, Day, Hour, Minute, Second, Hundredths;
+  time_t gps_t;
+  tmElements_t tm_ele;
+  float flat, flon;
+  //  Serial.println("loop()");
+  while(!rtc_is_set() && 
+	gps_is_active()){
+    // feedgps(2); // BROKEN
+    // Serial.println("while..");
+    gps.reset();
+    for(int i=0; i<100; i++){
+      for(int j=0; j<100; j++){
+	feedgps();
+      }
+    }
+    gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, &Hundredths, &age);
+    gps.f_get_position(&flat, &flon, &age);
+    
+    if(Year > 2000 && age < 10000){
+      // Serial.println("set time");
+      
+      lat = flat;
+      lon = flon;
+      tm_ele.Year = CalendarYrToTm(Year);
+      tm_ele.Month = Month;
+      tm_ele.Day = Day;
+      tm_ele.Hour = Hour;
+      tm_ele.Minute = Minute;
+      tm_ele.Second = Second;
 
-  //xxSerial.print("loop() ");
-  //xxSerial.print(year());
-  //xxSerial.print("/");
-  //xxSerial.print(month());
-  //xxSerial.print("/");
-  //xxSerial.print(day());
-  //xxSerial.print(" ");
-  Serial.print(hour());
-  Serial.print(":");
-  Serial.println(minute());
-  //\\Serial.print("lat:");
-  //\\Serial.println(lat);
-  feedgps(10);
+      gps_t = makeTime(tm_ele);
+      
+      time_t local_t = gps_t + local_offset(Year, gps_t, lat, lon);
+
+      setRTC(local_t);
+      setTime(local_t);
+      Serial.print("Clock Syncrhonized with GPS: Pos is ");
+      Serial.print(lat);
+      Serial.print(" ");
+      Serial.print(lon);
+      Serial.print(", Local Time is "); Serial.print(hour()); 
+      Serial.print(":"); Serial.print(minute()); 
+      Serial.print(":"); Serial.println(second());
+      rtc_set_hour = hour();
+    }
+  }
+////////////////////////////////////////////////////////////////////////////////
+  // gpsdump(gps);
+
+  delay(1000);
+  wdt_reset();  
 }
 
 void do_nothing(){
 }
 bool feedgps(uint8_t secs){
-  for(int i=0; i  < secs * 100; i++){
-    feedgps();
-    delay(10);
+  bool out = false;
+  // ignore overflow
+  unsigned long stop_ms = millis() + 1000 * secs;
+  Serial.println(stop_ms);
+  while(millis() < stop_ms){
+    Serial.println("HERE1");
+    for(int i=0; i < 1000; i++){
+      out = out || feedgps();
+    }
+    Serial.println("HERE2");
   }
+  Serial.println(out);
+  return out;
 }
 
 bool feedgps(){
+  char c;
   while (sws.available())
   {
-    if (gps.encode(sws.read()))
+    c = sws.read();
+    if (gps.encode(c)){
       return true;
+    }
   }
   return false;
+}
+
+void gpsdump(TinyGPS &gps)
+{
+  long lat, lon;
+  float flat, flon;
+  unsigned long age, date, time, chars;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned short sentences, failed;
+
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  Serial.print("GMT Date: "); Serial.print(static_cast<int>(month)); Serial.print("/"); Serial.print(static_cast<int>(day)); Serial.print("/"); Serial.print(year);
+  Serial.print("GMT Time: "); Serial.print(static_cast<int>(hour)); Serial.print(":"); Serial.print(static_cast<int>(minute)); Serial.print(":"); Serial.print(static_cast<int>(second)); Serial.print("."); Serial.print(static_cast<int>(hundredths));
+  Serial.print("  Fix age: ");  Serial.print(age); Serial.println("ms.");
 }
 
 void printFloat(double number, int digits)
